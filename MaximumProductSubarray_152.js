a833c8851b8a9ce7b402fb5b390c63f4f1394af8
Mykhailo Sheptun <michael.sheptun@gmail.com>
2021-07-16 19:24:13 -0400

    MaximumProductSubarray

---
+++ MaximumProductSubarray_152.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let maxTillIndex = [nums[0]]
+    let minTillIndex = [nums[0]]
+    let max = nums[0]
+
+    for(let i = 1; i < nums.length; i++){
+        const num = nums[i]
+
+        maxTillIndex[i] = Math.max(num, num * maxTillIndex[i - 1], num * minTillIndex[i - 1])
+        minTillIndex[i] = Math.min(num, num * maxTillIndex[i - 1], num * minTillIndex[i - 1])
+        max = Math.max(max, maxTillIndex[i])
+    }
+    return max
+};
