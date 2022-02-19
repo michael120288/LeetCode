55ba6d67d59ee05937b349698db2d16a98203e2b
Mykhailo Sheptun <michael.sheptun@gmail.com>
2022-02-18 22:11:07 -0500

    array except self

---
+++ Product_of_Array_except_self_238.js
@@ -0,0 +1,15 @@
+function productExpect(nums){
+    let output = new Array(nums.length).fill(nums[0])
+    let cacheVar = nums[nums.length -1]
+
+    for (let i = 1; i < nums.length; i++){
+        output[i] = output[i-1]*nums[i]
+    }
+    output[nums.length -1] = output[output.length - 2]
+    for (let j = nums.length -2; j > 0; j--){
+        output[j] = output[j-1]*cacheVar
+        cacheVar = cacheVar*nums[j]
+    }
+    output[0] = cacheVar
+    return output
+}
