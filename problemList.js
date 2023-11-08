cc8ee7a50c1968378ec940aca17cfb6130b8f818
Michael <michael.sheptun@gmail.com>
2023-11-08 14:25:40 -0500

    ProblemList

---
+++ problemList.js
@@ -0,0 +1,19 @@
+var createCounter = function(init) {
+    let num = init;
+
+
+    return {
+        increment: () => {
+            num++;
+            return num;
+        },
+        reset: () => {
+            num = init;
+            return num;
+        },
+        decrement: () => {
+            num--;
+            return num;
+        }
+    }
+};
